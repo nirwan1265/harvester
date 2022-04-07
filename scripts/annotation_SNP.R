e886af00ba24e29f92133e77540ac45740e99230
nirwan1265 <tandukar.nirwan@gmail.com>
2022-04-07 09:58:06 -0400

    MLM

--- scripts/annotation_SNP.R
+++ scripts/annotation_SNP.R
@@ -79,15 +79,16 @@ colnames(snp.db) <- c("Chromosome","Database","Region","Start","End","NA","Stran
 ##Loading query SNPs for GWAS from RDS file
 ##Location in the server: /rsstu/users/r/rrellan/sara/SorghumGEA/results/GLM_20220222 - GLM
 ##Location in the server: /rsstu/users/r/rrellan/sara/SorghumGEA/results/GLM_20220224 - GLM
-setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Data for sorghum/sorghum/GWAS.results")
+setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Data for sorghum/sorghum/GWAS.results/MLM")
 ##Reading the gwas RDS files
 for(i in sprintf("%02d", 1:10)){
-  assign(paste0("query.snp.gwas", i) , readRDS(file = paste0("glm_sol_VL_",i,"_20220224_03_09.RDS")))
+  assign(paste0("query.snp.gwas", i) , readRDS(file = paste0("mlm_sol_VL_",i,"_20220307_19_13.RDS")))
 }
+
 ##Subsetting the required columns for analysis
 for(i in sprintf("%02d", 1:10)){
-  assign(paste0("query.snp.gwas",i), data.frame(get(paste0("query.snp.gwas",i))[1]))
-  assign(paste0("query.snp.gwas",i), get(paste0("query.snp.gwas",i))[,c(2,3,4,5,6)])
+  assign(paste0("query.snp.gwas",i), data.frame(get(paste0("query.snp.gwas",i))[3]))
+  assign(paste0("query.snp.gwas",i), get(paste0("query.snp.gwas",i))[,c(2,3,4,6,7)])
 }
 
 ##Sub-setting chromosomes from gene annotation database
