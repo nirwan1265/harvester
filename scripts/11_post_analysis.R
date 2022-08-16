724bab79244f52992ccf22ee1a74c0acfbb48c91
nirwan1265 <tandukar.nirwan@gmail.com>
2022-08-16 14:46:22 -0400

    update venn

--- scripts/11_post_analysis.R
+++ scripts/11_post_analysis.R
@@ -1,3 +1,9 @@
+library(ggvenn)
+# Recover data
+setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Research/Results/pvalues.combination")
+
+
+
 ##Plotting the number of SNPs for each gene
 par(mfrow = c(5, 5))
 
@@ -19,19 +25,85 @@ for(i in paste0("gwas",sprintf("%02d", 1:10),".Marker")){
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+# Read the files
+for(i in sprintf("%02d", 1:10)){
+  assign(paste0("pvalue.combine",i), read.csv(paste0("pvalue.combine",i,".csv")))
+}
+j <- 1
+for(i in paste0("pvalue.combine",sprintf("%02d", 1:10))){
+  d = get(i)
+  colnames(d) <- c("Gene","GBJ","GHC","minP","SKAT","OMNI_ItoIV","CCT_ItoIV")
+  assign(i,d)
+}
 
 #Filtering only the significant genes
 filtered_genes_OMNI <- NULL
 filtered_genes_CCT <- NULL
 filtered_genes_GBJ <- NULL
+filtered_genes_SKAT <- NULL
+filtered_genes_GHC <- NULL
+filtered_genes_minP <- NULL
 for(i in paste0("pvalue.combine",sprintf("%02d", 1:10))){
   d = get(i)
-  x <- dplyr::filter(d, OMNI_ItoIV < 0.05)
-  y <- dplyr::filter(d, CCT_ItoIV < 0.05)
-  z <- dplyr::filter(d, GBJ < 0.05)
-  filtered_genes_OMNI <- rbind(filtered_genes_OMNI,x)
-  filtered_genes_CCT <- rbind(filtered_genes_CCT,y)
-  filtered_genes_GBJ <- rbind(filtered_genes_GBJ,z)
+  u <- dplyr::filter(d, GBJ < 0.05)
+  v <- dplyr::filter(d, GHC < 0.05)
+  w <- dplyr::filter(d, minP < 0.05)
+  x <- dplyr::filter(d, SKAT < 0.05)
+  y <- dplyr::filter(d, OMNI_ItoIV < 0.05)
+  z <- dplyr::filter(d, CCT_ItoIV < 0.05)
+  filtered_genes_GBJ <- rbind(filtered_genes_GBJ,u)
+  filtered_genes_GHC <- rbind(filtered_genes_GHC,v)
+  filtered_genes_minP <- rbind(filtered_genes_minP,w)
+  filtered_genes_SKAT <- rbind(filtered_genes_SKAT,x)
+  filtered_genes_OMNI <- rbind(filtered_genes_OMNI,y)
+  filtered_genes_CCT <- rbind(filtered_genes_CCT,z)
   assign(i,d)
 }
 
+
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+#Filtered genes:
+filtered_genes_OMNI <- read.csv("filtered_genes_OMNI.csv")
+filtered_genes_CCT <- read.csv("filtered_genes_CCT.csv")
+filtered_genes_GBJ <- read.csv("filtered_genes_GBJ.csv")
+
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+# Venn diagram
+x <- list(
+  GBJ = as.vector(filtered_genes_GBJ[,1]),
+  GHC = as.vector(filtered_genes_GHC[,1]),
+  minP = as.vector(filtered_genes_minP[,1]),
+  SKAT = as.vector(filtered_genes_SKAT[,1]),
+  OMNI <- as.vector(filtered_genes_OMNI[,1]),
+  CCT <- as.vector(filtered_genes_CCT[,1])
+)
+
+ggvenn(
+  x,
+  fill_color = c("#0073C2FF", "#EFC000FF", "#868686FF", "#CD534CFF","#CD268CFF","#EFC000FF"),
+  stroke_size = 0.5, set_name_size = 4
+)
+
+set.seed(20190708)
+genes <- paste("gene",1:1000,sep="")
+
+x <- list(
+  A = sample(genes,300), 
+  B = sample(genes,525), 
+  C = sample(genes,440),
+  D = sample(genes,350)
+)
+x
+A = sample(genes,300)
+typeof(A)
+typeof(GBJ)
+class(GBJ)
