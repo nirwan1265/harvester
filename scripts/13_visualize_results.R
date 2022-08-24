ad73e216ba3c598cb985f153464179446a641b2f
nirwan1265 <tandukar.nirwan@gmail.com>
2022-08-24 11:02:14 -0400

    visualize data

--- scripts/13_visualize_results.R
+++ scripts/13_visualize_results.R
@@ -132,7 +132,7 @@ go.bp.res %>%
   ggplot(aes(Description,Count))+
   geom_col() +
   coord_flip()+
-  labs(title="Raw GWAS GO_BP",
+  labs(title="Raw GWAS enriched Biological Processes",
        x="Gene Count", y= "Description")+
   geom_text(aes(label = round(Count, 1)), nudge_y= -1, color="white")
 
@@ -145,7 +145,7 @@ go.mf.res %>%
   ggplot(aes(Description,Count))+
   geom_col() +
   coord_flip()+
-  labs(title="Raw GWAS GO_MF",
+  labs(title="Raw GWAS enriched Molecular Functions",
        x="Gene Count", y= "Description")+
   geom_text(aes(label = round(Count, 1)), nudge_y= -1, color="white")
 
@@ -158,8 +158,121 @@ go.cc.res %>%
   ggplot(aes(Description,Count))+
   geom_col() +
   coord_flip()+
-  labs(title="Raw GWAS GO_CC",
+  labs(title="Raw GWAS enriched Cellular Components",
        x="Gene Count", y= "Description")+
   geom_text(aes(label = round(Count, 1)), nudge_y= -1, color="white")
 
 
+
+################################################################################
+################################################################################
+
+# For OMNI test:
+
+## Read Data:
+omni_genename_geneid <- read.csv("OMNI_genename_uniprot_geneid.csv")
+colnames(omni_genename_geneid)[1] <- "Gene"
+omni <- filtered_genes_OMNI[,c(1,6)]
+
+# Changing gene name
+omni_geneid <- inner_join(omni_genename_geneid, omni)
+
+## Here a gene,C5XPA5, has several different transcripts with different geneid but same uniprot name. 
+## have to put the pvalues individually for them
+#Haven't done it here due to time constraint for the poster on Friday aug 23rd.
+
+
+#GO analysis
+
+#Sorting the data
+## feature 1: numeric vector
+#geneList = rnaseq_analysis[,2]
+geneList = omni_geneid[,4]
+
+## feature 2: named vector
+#names(geneList) = as.character(rnaseq_analysis[,1])
+#geneList
+names(geneList) = as.character(omni_geneid[,3])
+geneList
+
+## feature 3: decreasing order
+geneList = sort(geneList, decreasing = TRUE)
+head(geneList)
+
+gene <-names(geneList)[geneList < 0.05 & geneList != 0]
+head(gene)
+
+
+
+
+# keytype
+# “ACCNUM” “ALIAS” “ENSEMBL” “ENSEMBLPROT” “ENSEMBLTRANS” “ENTREZID”
+# “ENZYME” “EVIDENCE” “EVIDENCEALL” “FLYBASE” “FLYBASECG” “FLYBASEPROT”
+# “GENENAME” “GO” “GOALL” “MAP” “ONTOLOGY” “ONTOLOGYALL”
+# “PATH” “PMID” “REFSEQ” “SYMBOL” “UNIGENE” “UNIPROT”
+
+#GO over-representation analysis
+ego_BP <- enrichGO(gene = gene,
+                   OrgDb         = org.Sbicolor.eg.db,
+                   keyType       = 'ENTREZID',
+                   ont           = "BP",
+                   pAdjustMethod = "BH",
+                   pvalueCutoff  = 0.01,
+                   qvalueCutoff  = 0.05)
+
+ego_MF <- enrichGO(gene = gene,
+                   OrgDb         = org.Sbicolor.eg.db,
+                   keyType       = 'ENTREZID',
+                   ont           = "MF",
+                   pAdjustMethod = "BH",
+                   pvalueCutoff  = 0.01,
+                   qvalueCutoff  = 0.05)
+
+ego_CC <- enrichGO(gene = gene,
+                   OrgDb         = org.Sbicolor.eg.db,
+                   keyType       = 'ENTREZID',
+                   ont           = "CC",
+                   pAdjustMethod = "BH",
+                   pvalueCutoff  = 0.01,
+                   qvalueCutoff  = 0.05)
+
+
+# Visualize
+go.bp.res <- as.data.frame(cbind(ego_BP@result[["Description"]],ego_BP@result[["Count"]],ego_BP@result[["pvalue"]]))
+colnames(go.bp.res) <- c("Description","Count","pvalue")
+go.bp.res[,3] <- sapply(go.bp.res[,3], as.numeric)
+go.bp.res <- go.bp.res[which(go.bp.res$pvalue < 0.05), ]
+go.bp.res[,2] <- sapply(go.bp.res[,2], as.double)
+go.bp.res %>% 
+  ggplot(aes(Description,Count))+
+  geom_col() +
+  coord_flip()+
+  labs(title="OMNI test enriched Biological Processes",
+       x="Gene Count", y= "Description")+
+  geom_text(aes(label = round(Count, 1)), nudge_y= -1, color="white")
+
+go.mf.res <- as.data.frame(cbind(ego_MF@result[["Description"]],ego_MF@result[["Count"]],ego_MF@result[["pvalue"]]))
+colnames(go.mf.res) <- c("Description","Count","pvalue")
+go.mf.res[,3] <- sapply(go.mf.res[,3], as.numeric)
+go.mf.res <- go.mf.res[which(go.mf.res$pvalue < 0.05), ]
+go.mf.res[,2] <- sapply(go.mf.res[,2], as.double)
+go.mf.res %>% 
+  ggplot(aes(Description,Count))+
+  geom_col() +
+  coord_flip()+
+  labs(title="OMNI test enriched Molecular Functions",
+       x="Gene Count", y= "Description")+
+  geom_text(aes(label = round(Count, 1)), nudge_y= -1, color="white")
+
+go.cc.res <- as.data.frame(cbind(ego_CC@result[["Description"]],ego_CC@result[["Count"]],ego_CC@result[["pvalue"]]))
+colnames(go.cc.res) <- c("Description","Count","pvalue")
+go.cc.res[,3] <- sapply(go.cc.res[,3], as.numeric)
+go.cc.res <- go.cc.res[which(go.cc.res$pvalue < 0.05), ]
+go.cc.res[,2] <- sapply(go.cc.res[,2], as.double)
+go.cc.res %>% 
+  ggplot(aes(Description,Count))+
+  geom_col() +
+  coord_flip()+
+  labs(title="OMNI test enriched Cellular Components",
+       x="Gene Count", y= "Description")+
+  geom_text(aes(label = round(Count, 1)), nudge_y= -1, color="white")
