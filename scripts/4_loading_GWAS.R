4f41188ef11309f90e509b7d60708bb7eb222724
nirwan1265 <tandukar.nirwan@gmail.com>
2022-08-31 11:24:15 -0400

    update

--- scripts/4_loading_GWAS.R
+++ scripts/4_loading_GWAS.R
@@ -13,4 +13,17 @@ for(i in sprintf("%02d", 1:10)){
   assign(paste0("query.snp.gwas",i), get(paste0("query.snp.gwas",i))[,c(2,3,4,6,7)])
 }
 
+## combining all the query gwas snps for magma
+raw.gwas.all.snps <- NULL
+for(i in paste0("query.snp.gwas",sprintf("%02d", 1:10))){
+  d = get(i)
+  raw.gwas.all.snps <- rbind(raw.gwas.all.snps,d)
+  assign(i,d)
+}
+## Removing None in Markers column
+raw.gwas.all.snps <-raw.gwas.all.snps[!grepl("None", raw.gwas.all.snps$MLM_Stats.Marker), ]
 
+## Saving only markers and pvalue
+raw.gwas.all.snps <- as.data.frame(raw.gwas.all.snps[,c(1,5)])
+setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Github/eMAGMA")
+write.table(raw.gwas.all.snps,"raw.gwas.all.snps.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
